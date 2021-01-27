e33da7030c27ac8b920a927fc2973de370af1a20
anhvupt <anhvupt2412@gmail.com>
2021-01-27 10:56:21 +0700

    client: refactor

---
+++ client/course-management/src/app/shared/back-button/back-button.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-back-button',
+  templateUrl: './back-button.component.html',
+  styleUrls: ['./back-button.component.css']
+})
+export class BackButtonComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() text: string
+
+  ngOnInit(): void {
+  }
+
+}
--- client/course-management/src/app/shared/shared.module.ts
+++ client/course-management/src/app/shared/shared.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationComponent } from './pagination/pagination.component';
+import { BackButtonComponent } from './back-button/back-button.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [PaginationComponent],
+  declarations: [PaginationComponent, BackButtonComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     CommonModule, 
     PaginationComponent, 
+    BackButtonComponent
   ]
 })
 export class SharedModule { }
